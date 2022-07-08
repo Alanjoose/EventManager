d85ff64f8fc96165f8300fb551d27eeb227ed357
Bruno <teste@gmail.com>
2022-07-08 15:21:00 -0300

    Add events_user_table

--- database/migrations/2022_07_08_182045_create_event_user_table.php
+++ database/migrations/2022_07_08_182045_create_event_user_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('events_users', function (Blueprint $table) {
+        Schema::create('event_user', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('events_users');
+        Schema::dropIfExists('event_user');
     }
 };
